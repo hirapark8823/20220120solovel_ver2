b31748921cf0651c5a42192494c0768a8b9b54ae
Itaru Sugimoto <sugimoto.itaru@moneyforward.co.jp>
2022-01-23 23:27:49 +0900

    ログイン後メイン画面に遷移
    ・ログインはFirebaseのAuthenticationに頼る
    ・ProviderIdを利用してユーザー判定する（本当はトークンがほしい）
    ・

--- solovel.ver3/LoginViewController.swift
+++ solovel.ver3/LoginViewController.swift
@@ -24,36 +24,23 @@ class LoginViewController: UIViewController{
             emailTextField.delegate = self
             passwordTextField.delegate = self
         }
-//        print("ok")
+        
         guard let email = emailTextField.text else{ return }
         guard let password = passwordTextField.text else{ return }
         
-        Auth.auth().signIn(withEmail: email, password: password) { (res, err ) in
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] res, err in
+            guard let self = self else { return }
+            
             if let err = err {
                 print("Login失敗", err)
                 return
             }
             print("Login成功")
             
-            guard let uid = Auth.auth().currentUser?.uid else { return }
-            let userRef = Firestore.firestore().collection("users").document(uid)
-            userRef.getDocument { (snapshot, err)in
-                if let err = err {
-                    print("ユーザー情報の取得に失敗しました。\(err)")
-                    return
-                }
-                
-                guard let data = snapshot?.data() else {return}
-                let user = User.init(dic: data)
-                
-                print("ユーザー情報の取得が出来ました。\(user.name)")
-                
-                //画面遷移
-                // let storyBoard = UIStoryboard(name: "timeline", bundle: nil)
-                // let homeViewController = storyBoard.instantiateViewController(withIdentifier: "timelineViewController")as! timelineViewController
-                // self.present(homeViewController, animated: true, completion: nil)
-                }
-                
+            UserDefaults.standard.set( res?.user.providerID, forKey: "loggedInUserId")
+            let tabBarController = self.storyboard?.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
+            self.present(tabBarController, animated: true, completion: nil)
+            
             }
         }
     }
